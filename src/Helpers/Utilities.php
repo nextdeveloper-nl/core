a534a3735f318572a9fce42ec581d8e68c3496da
Semih Turna <semih.turna@plusclouds.com>
2018-05-25 21:48:55 +0300

    Güncelleme#1

--- src/Helpers/Utilities.php
+++ src/Helpers/Utilities.php
@@ -40,13 +40,21 @@ function customRnd($min = 1, $max = 2) {
  * @return string
  */
 function genUuid() {
-    $rnd = unpack( 'v*', fread( fopen( '/dev/random', 'r' ), 16 ) );
-
-    $uuid = sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-        $rnd[1], $rnd[2], $rnd[3], $rnd[4] & 0x0fff | 0x4000,
-        $rnd[5] & 0x3fff | 0x8000, $rnd[6], $rnd[7], $rnd[8] );
-
-    return $uuid;
+    return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        // 32 bits for "time_low"
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+        // 16 bits for "time_mid"
+        mt_rand( 0, 0xffff ),
+        // 16 bits for "time_hi_and_version",
+        // four most significant bits holds version number 4
+        mt_rand( 0, 0x0fff ) | 0x4000,
+        // 16 bits, 8 bits for "clk_seq_hi_res",
+        // 8 bits for "clk_seq_low",
+        // two most significant bits holds zero and one for variant DCE1.1
+        mt_rand( 0, 0x3fff ) | 0x8000,
+        // 48 bits for "node"
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+    );
 }
 
 /**
