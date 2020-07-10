ddf861b986b97ce9e6829211269fb0eae6342bf0
heisenberg <semih.turna@plusclouds.com>
2020-07-10 13:41:59 +0300

    ICoreException interface added

---
+++ src/Exceptions/ICoreException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of the PlusClouds.Core library.
+ *
+ * (c) Semih Turna <semih.turna@plusclouds.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PlusClouds\Core\Exceptions;
+
+interface ICoreException {
+}
