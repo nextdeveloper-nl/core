98d0ca2c4e4610d18e9f42ef717ecb149e4884b2
Harun Baris Bulut <baris.bulut@plusclouds.com>
2020-10-09 14:37:50 +0300

    Line fix made

--- src/Common/Services/NiN/NiN.php
+++ src/Common/Services/NiN/NiN.php
@@ -10,7 +10,6 @@
 
 namespace PlusClouds\Core\Common\Services\NiN;
 
-
 /**
  * Class NiN
  * @package PlusClouds\Core\Common\Services\NiN
@@ -40,16 +39,15 @@ class NiN
      *
      * @return bool
      */
-    public function verify($fields, $citizen = true) {
+    public function verify($fields, $citizen = true)
+    {
         $result = false;
 
         try {
-            $this->checkRequiredFields( $fields, $citizen );
+            $this->checkRequiredFields($fields, $citizen);
 
-            $result = $citizen ? $this->citizen( $fields ) : $this->foreignCitizen( $fields );
-        }
-        catch( \InvalidArgumentException $e ) {
-
+            $result = $citizen ? $this->citizen($fields) : $this->foreignCitizen($fields);
+        } catch (\InvalidArgumentException $e) {
         }
 
         return $result;
@@ -60,17 +58,16 @@ class NiN
      *
      * @return bool
      */
-    private function citizen(array $fields) {
-        $client = new \SoapClient( $this->endpoints['v1'], [ 'soap_version' => SOAP_1_2 ] );
+    private function citizen(array $fields)
+    {
+        $client = new \SoapClient($this->endpoints['v1'], [ 'soap_version' => SOAP_1_2 ]);
 
-        $response = $client->TCKimlikNoDogrula( [
+        $response = $client->TCKimlikNoDogrula([
             'TCKimlikNo' => $fields['nin'],
-            'Ad'         => upperCaseTr( $fields['name'] ),
-            'Soyad'      => upperCaseTr( $fields['surname'] ),
+            'Ad'         => upperCaseTr($fields['name']),
+            'Soyad'      => upperCaseTr($fields['surname']),
             'DogumYili'  => $fields['year'],
-        ] );
-
-        logger()->info('TCKimlik doğrulama sonucu: ' . print_r($response, true));
+        ]);
 
         return (bool) $response->TCKimlikNoDogrulaResult;
     }
@@ -80,17 +77,18 @@ class NiN
      *
      * @return bool
      */
-    private function foreignCitizen(array $fields) {
-        $client = new \SoapClient( $this->endpoints['v2'], [ 'soap_version' => SOAP_1_2 ] );
+    private function foreignCitizen(array $fields)
+    {
+        $client = new \SoapClient($this->endpoints['v2'], [ 'soap_version' => SOAP_1_2 ]);
 
-        $response = $client->YabanciKimlikNoDogrula( [
+        $response = $client->YabanciKimlikNoDogrula([
             'KimlikNo' => $fields['nin'],
-            'Ad'       => upperCaseTr( $fields['name'] ),
-            'Soyad'    => upperCaseTr( $fields['surname'] ),
+            'Ad'       => upperCaseTr($fields['name']),
+            'Soyad'    => upperCaseTr($fields['surname']),
             'DogumGun' => $fields['day'],
             'DogumAy'  => $fields['month'],
             'DogumYil' => $fields['year'],
-        ] );
+        ]);
 
         return (bool) $response->YabanciKimlikNoDogrulaResult;
     }
@@ -101,18 +99,18 @@ class NiN
      *
      * @return bool
      */
-    private function checkRequiredFields($fields, $citizen) {
-        $keys = array_keys( $fields );
+    private function checkRequiredFields($fields, $citizen)
+    {
+        $keys = array_keys($fields);
 
         $type = (int) ! $citizen;
 
-        foreach( $this->requiredFields[ $type ] as $key ) {
-            if( ! in_array( $key, $keys ) ) {
+        foreach ($this->requiredFields[ $type ] as $key) {
+            if (! in_array($key, $keys)) {
                 throw new \InvalidArgumentException();
             }
         }
 
         return true;
     }
-
 }
