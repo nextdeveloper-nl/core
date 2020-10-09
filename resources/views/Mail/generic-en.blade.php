611e4040f66bf199152a20012d04084ac1c04449
Harun Baris Bulut <baris.bulut@plusclouds.com>
2020-10-09 16:13:47 +0300

    Added generic en template

---
+++ resources/views/Mail/generic-en.blade.php
@@ -0,0 +1,322 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <!--[if !mso]><!-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <!--<![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>PlusClouds</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
+
+    <script
+            src="https://kit.fontawesome.com/120faeb1b4.js"
+            crossorigin="anonymous"
+    ></script>
+
+    <style type="text/css">
+        html,
+        body {
+            font-family: "Poppins", sans-serif;
+            font-weight: 400;
+            color: #ffffff;
+            background-color: #0a1f44;
+            font-size: 13px;
+            line-height: 20px;
+            -webkit-text-size-adjust: none;
+            -ms-text-size-adjust: none;
+            margin: 0;
+            padding: 0;
+        }
+
+        .f-icon {
+            font-size: 50px !important;
+            color: #00baf2;
+        }
+
+        a {
+            text-decoration: none !important;
+        }
+
+        a.browser {
+            font-size: 12px;
+            color: #ffffff;
+        }
+
+        a.unsubscribe {
+            color: #b5b5c3;
+        }
+
+        table {
+            border-spacing: 0;
+            table-layout: auto;
+            margin: 0 auto;
+        }
+
+        h2 {
+            font-weight: 500;
+            font-size: 18px;
+        }
+
+        h2 span {
+            font-weight: 700;
+        }
+
+        .body-text {
+            padding: 10px 35px;
+            color: #1b283f;
+        }
+
+        .body-text a {
+            text-decoration: underline !important;
+            color: #1b283f;
+        }
+
+        .body-text p {
+            padding: 10px 0px;
+        }
+
+        a.plusclouds {
+            color: #00baf2;
+            text-decoration: none !important;
+        }
+
+        /*Responsive*/
+        @media only screen and (max-width: 640px) {
+            body {
+                margin: 0px;
+                width: auto !important;
+            }
+
+            .table-inner {
+                width: 90% !important;
+                max-width: 90% !important;
+            }
+
+            .table-full {
+                width: 95% !important;
+                max-width: 95% !important;
+            }
+        }
+
+        @media only screen and (max-width: 479px) {
+            body {
+                width: auto !important;
+            }
+
+            .table-inner {
+                width: 90% !important;
+            }
+
+            .table-full {
+                width: 95% !important;
+                max-width: 95% !important;
+            }
+
+            /*gmail*/
+            u + .body .full {
+                width: 95% !important;
+                width: 95vw !important;
+            }
+        }
+    </style>
+</head>
+<body class="body" style="background-color: #0a1f44;">
+<!--logo-->
+<table
+        align="center"
+        width="100%"
+        border="0"
+        cellspacing="0"
+        cellpadding="0"
+>
+    <tr>
+        <td align="center">
+            <table
+                    align="center"
+                    class="table-full"
+                    width="680"
+                    style="max-width: 680px"
+                    border="0"
+                    cellspacing="0"
+                    cellpadding="0"
+            >
+                <tr>
+                    <td height="50"></td>
+                </tr>
+                <tr>
+                    <td>
+                        <!--center-logo-->
+                        <table
+                                border="0"
+                                align="center"
+                                cellpadding="0"
+                                cellspacing="0"
+                                class="table-full"
+                        >
+                            <tr>
+                                <td align="center">
+                                    <a href="#"
+                                    ><img
+                                                style="
+                            display: block;
+                            line-height: 0px;
+                            font-size: 0px;
+                            border: 0px;
+                          "
+                                                src="https://plusclouds.com.tr/assets/dark.png"
+                                                alt="plusclouds-logo"
+                                        /></a>
+                                </td>
+                            </tr>
+                        </table>
+                        <!--end center-logo-->
+                    </td>
+                </tr>
+                <tr>
+                    <td height="50"></td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td height="25"></td>
+    </tr>
+</table>
+<!--end logo-->
+
+<!--body-->
+<table
+        align="center"
+        width="100%"
+        border="0"
+        cellspacing="0"
+        cellpadding="0"
+>
+    <tr>
+        <td align="center">
+            <table
+                    align="center"
+                    class="table-full"
+                    width="680"
+                    style="max-width: 680px; border-radius: 10px"
+                    border="0"
+                    cellspacing="0"
+                    cellpadding="0"
+                    bgcolor="#FFFFFF"
+            >
+                <tr>
+                    <td height="30"></td>
+                </tr>
+                <tr>
+                    <td>
+                        <!--body-text-->
+                        <table
+                                border="0"
+                                cellpadding="0"
+                                cellspacing="0"
+                                width="680"
+                                style="max-width: 680px; "
+                                class="table-full"
+                        >
+                            <tr>
+                                <td class="body-text">
+                                    <i class="far fa-handshake f-icon"></i>
+                                    {!! $template !!}
+                                </td>
+                            </tr>
+                        </table>
+                        <!--end body-text-->
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td height="25"></td>
+    </tr>
+</table>
+<!--end body-->
+
+<!--footer-->
+<table
+        align="center"
+        width="100%"
+        border="0"
+        cellspacing="0"
+        cellpadding="0"
+>
+    <tr>
+        <td align="center">
+            <table
+                    align="center"
+                    class="table-full"
+                    width="680"
+                    style="max-width: 680px"
+                    border="0"
+                    cellspacing="0"
+                    cellpadding="0"
+            >
+                <tr>
+                    <td height="30"></td>
+                </tr>
+                <tr>
+                    <td>
+                        <!--copyright-->
+                        <table
+                                border="0"
+                                align="center"
+                                cellpadding="0"
+                                cellspacing="0"
+                                class="table-full"
+                        >
+                            <tr>
+                                <td align="center" style="color:#ffffff">
+                                    © 2013-2020 PlusClouds - Şef Bilişim Hizmetleri A.Ş. All rights reserved
+                                </td>
+                            </tr>
+                        </table>
+                        <!--end copyright-->
+                    </td>
+                </tr>
+                <tr>
+                    <td height="10"></td>
+                </tr>
+                <tr>
+                    <td>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td height="40"></td>
+                </tr>
+                <tr>
+                    <td>
+                        <!--unsubscribe link-->
+                        <table
+                                border="0"
+                                align="center"
+                                cellpadding="0"
+                                cellspacing="0"
+                                class="table-full"
+                        >
+                            <tr>
+                                <td align="center" style="color: #b5b5c3">
+                                    This is an informative e-mail. You dont need to unsubscribe.
+                                </td>
+                            </tr>
+                        </table>
+                        <!--end unsubscribe link-->
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td height="25"></td>
+    </tr>
+</table>
+<!--end footer-->
+</body>
+</html>
