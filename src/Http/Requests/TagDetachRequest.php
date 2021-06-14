124e461f6df071c018ab0f90b31c5b19e55e3f72
Harun Baris Bulut <baris.bulut@plusclouds.com>
2021-06-14 18:26:20 +0300

    Tag attach, detach added

---
+++ src/Http/Requests/TagDetachRequest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of the PlusClouds.Core library.
+ *
+ * (c) Semih Turna <semih.turna@plusclouds.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PlusClouds\Core\Http\Requests;
+
+
+use BenSampo\Enum\Rules\EnumValue;
+use PlusClouds\Core\Common\Enums\TagType;
+
+/**
+ * Class TagStoreRequest
+ * @package PlusClouds\Core\Http\Requests
+ */
+class TagDetachRequest extends AbstractFormRequest
+{
+
+    /**
+     * @return bool
+     */
+    public function authorize() {
+        return true;
+    }
+
+    /**
+     * @return array
+     */
+    public function rules() {
+        return [
+            'name'        => 'required|max:50',
+            'object'      => 'required|string'
+        ];
+    }
+
+}
