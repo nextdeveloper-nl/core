f8214e604ef6bb9bd00121871994339745cb4a39
heisenberg <semih.turna@plusclouds.com>
2021-11-10 21:44:14 +0300

    namespace fix

--- src/Database/Traits/FullTextSearch.php
+++ src/Database/Traits/FullTextSearch.php
@@ -1,15 +1,20 @@
 <?php
+/**
+ * This file is part of the PlusClouds.Core library.
+ *
+ * (c) Semih Turna <recai.atak@plusclouds.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-namespace PlusClouds\CRM\Helpers\Traits;
+namespace PlusClouds\Core\Database\Traits;
 
-trait FullTextSearch
-{
+trait FullTextSearch {
+    public function scopeFullTextSearch($query, $term) {
+        $str = implode(',', $this->fullTextFields);
+        $query->whereRaw("MATCH ({$str}) AGAINST (? IN BOOLEAN MODE)", fullTextWildcards($term));
 
-    public function scopeFullTextSearch($query, $term)
-    {
-        $str = implode(',',$this->fullTextFields);
-        $query->whereRaw("MATCH ($str) AGAINST (? IN BOOLEAN MODE)" , fullTextWildcards($term));
         return $query;
     }
-
 }
