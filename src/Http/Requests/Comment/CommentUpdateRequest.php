e4d07a3e8b682f3d34f339b67e67a6db9e25e4c3
atak011 <atakrecai@gmail.com>
2021-12-07 13:22:24 +0300

    requests added

---
+++ src/Http/Requests/Comment/CommentUpdateRequest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This file is part of the PlusClouds.Core library.
+ *
+ * (c) Semih Turna <semih.turna@plusclouds.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PlusClouds\Core\Http\Requests\Comment;
+
+use PlusClouds\Core\Http\Requests\AbstractFormRequest;
+
+/**
+ * Class CategoryStoreRequest.
+ *
+ * @package PlusClouds\Core\Http\Requests
+ */
+class CommentUpdateRequest extends AbstractFormRequest
+{
+    /**
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'comment'  =>  'required|string|max:255'
+        ];
+    }
+}
