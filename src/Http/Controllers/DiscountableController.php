1535a0b1dc85d8289f5f15d0d03d84810ad64836
atak011 <atakrecai@gmail.com>
2021-12-07 13:23:07 +0300

    controller created

---
+++ src/Http/Controllers/DiscountableController.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This file is part of the PlusClouds.Core library.
+ *
+ * (c) Semih Turna <semih.turna@plusclouds.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PlusClouds\Core\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PlusClouds\Core\Database\Models\Comment;
+use PlusClouds\Core\Http\Requests\Comment\MetaStoreRequest;
+use PlusClouds\Core\Http\Requests\Comment\CommentUpdateRequest;
+
+/**
+ * Class DiscountableController.
+ *
+ * @package PlusClouds\Core\Http\Controllers
+ */
+class DiscountableController extends AbstractController
+{
+
+    public function store()
+    {
+
+    }
+
+
+    public function update()
+    {
+
+    }
+
+
+    public function destroy()
+    {
+
+    }
+
+
+}
