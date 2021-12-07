47236a66a7a29a6fc547bafb43eb716ad2008d78
atak011 <atakrecai@gmail.com>
2021-12-07 15:20:01 +0300

    model created

---
+++ src/Database/Models/Discountable.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the PlusClouds.Core library.
+ *
+ * (c) Semih Turna <semih.turna@plusclouds.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PlusClouds\Core\Database\Models;
+
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use PlusClouds\Core\Database\Traits\Filterable;
+
+/**
+ * Class Taggables
+ * @package PlusClouds\Core\Database\Models
+ */
+class Discountable extends AbstractModel
+{
+
+
+    /**
+     * @var array
+     */
+    protected $guarded = [];
+
+}
