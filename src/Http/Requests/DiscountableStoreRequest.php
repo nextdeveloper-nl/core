6890212bc2771ea45eafab9052488b9c39ec786d
atak011 <atakrecai@gmail.com>
2021-12-07 15:20:20 +0300

    request created

---
+++ src/Http/Requests/DiscountableStoreRequest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of the PlusClouds.Core library.
+ *
+ * (c) Semih Turna <semih.turna@plusclouds.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PlusClouds\Core\Http\Requests;
+
+use PlusClouds\Core\Http\Requests\AbstractFormRequest;
+
+/**
+ * Class CategoryStoreRequest.
+ *
+ * @package PlusClouds\Core\Http\Requests
+ */
+class DiscountableStoreRequest extends AbstractFormRequest
+{
+    /**
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'discount_id'  =>  'required|string',
+            'object'       =>  'required|string',
+            'object_id'    =>  'required|string',
+        ];
+    }
+}
