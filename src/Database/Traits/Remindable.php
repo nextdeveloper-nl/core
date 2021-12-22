c701421461e338f03a3ad79949d99757b9a7b865
atak011 <atakrecai@gmail.com>
2021-12-23 01:42:24 +0300

    trait created

---
+++ src/Database/Traits/Remindable.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of the PlusClouds.Core library.
+ *
+ * (c) Semih Turna <semih.turna@plusclouds.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PlusClouds\Core\Database\Traits;
+
+
+
+/**
+ * Trait .
+ *
+ * @package PlusClouds\Core\Database\Traits
+ */
+trait Remindable {
+
+}
