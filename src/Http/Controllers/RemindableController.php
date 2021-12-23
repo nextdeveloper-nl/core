fc6b3ef27c38b622cc7cf268a1c06905f0e1874a
atak011 <atakrecai@gmail.com>
2021-12-24 01:17:23 +0300

    controller created

---
+++ src/Http/Controllers/RemindableController.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * This file is part of the PlusClouds.Core library.
+ *
+ * (c) Semih Turna <semih.turna@plusclouds.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PlusClouds\Core\Http\Controllers;
+
+
+use PlusClouds\Core\Database\Models\Remindable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use PlusClouds\CRM\Database\Models\Organization;
+use PlusClouds\Core\Http\Requests\Address\RemindableStoreRequest;
+use PlusClouds\Core\Http\Requests\Address\RemindableUpdateRequest;
+
+/**
+ * Class AddressController
+ * @package PlusClouds\Core\Http\Controllers
+ */
+class RemindableController extends AbstractController
+{
+
+    public function store(RemindableStoreRequest $request)
+    {
+        $data = $request->validated();
+
+        $objectArr = findObjectFromClassName($data['remindable_object'], $data['remindable_id'], 'Remindable');
+
+        Remindable::create([
+            'note'                   => $data['note'] ?? null,
+            'remindable_id'          => $objectArr[1],
+            'remindable_object_type' => $objectArr[0],
+            'remind_datetime'        => $data['remind_datetime'],
+            'user_id'                => getAUUser()->id,
+        ]);
+
+        return $this->noContent();
+    }
+
+    public function update(RemindableUpdateRequest $request, Remindable $remindable)
+    {
+        $data = $request->validated();
+
+        if ($request->has('snooze_datetime')){
+            $data['status'] = 3;
+        }
+
+        $remindable->update($data);
+
+        return $this->noContent();
+    }
+
+
+    public function destroy(Remindable $remindable)
+    {
+        $remindable->delete();
+        return $this->noContent();
+    }
+}
