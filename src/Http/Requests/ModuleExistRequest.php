e863db9c7d99123641c02995d78ad093ec4ed45e
atakrecai <atakreca@gmail.com>
2021-12-24 17:53:46 +0300

    Request Created

---
+++ src/Http/Requests/ModuleExistRequest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This file is part of the PlusClouds.Core library.
+ *
+ * (c) Semih Turna <semih.turna@plusclouds.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PlusClouds\Core\Http\Requests;
+
+
+/**
+ * Class MailBatchSendRequest
+ * @package PlusClouds\Core\Http\Requests
+ */
+class ModuleExistRequest extends AbstractFormRequest
+{
+
+    /**
+     * @return bool
+     */
+    public function authorize() {
+        return true;
+    }
+
+    /**
+     * @return array
+     */
+    public function rules() {
+        return [
+            'name' => 'required|string'
+        ];
+    }
+
+}
