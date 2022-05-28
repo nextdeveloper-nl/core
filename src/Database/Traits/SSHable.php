9cab10582993f039bd59bd76aebfe4de18795fd4
Semih Yonet <semihyonet@gmail.com>
2022-05-28 17:28:12 +0200

    Auto stash before checking out "origin/dev"

--- src/Database/Traits/SSHable.php
+++ src/Database/Traits/SSHable.php
@@ -4,6 +4,7 @@ namespace PlusClouds\Core\Database\Traits;
 
 use Bravo3\SSH\Connection;
 use Bravo3\SSH\Credentials\PasswordCredential;
+use PlusClouds\Core\Common\Logger\QueueLogger;
 use PlusClouds\Core\Exceptions\CannotConnectWithSSHException;
 
 /*
@@ -12,11 +13,35 @@ use PlusClouds\Core\Exceptions\CannotConnectWithSSHException;
 
 trait SSHable
 {
-    public function performSSHCommand(string $command)
+    public function performSSHCommand(string $command, QueueLogger $logger = null)
     {
-        $connection = $this->createSSHConnection();
-
+        $connection = $this->createSSHConnection($logger);
+        if (!is_null($logger)) {
+            $logger->info("Executing : " . $command);
+        }
         $result = trim($connection->execute($command)->getOutput());
+        if (!is_null($logger)) {
+            $logger->info("Result : " . $result);
+        }
+        $connection->disconnect();
+        return $result;
+    }
+
+    public function performSSHCommands(array $commands, QueueLogger $logger = null)
+    {
+        $connection = $this->createSSHConnection($logger);
+
+        $result = "";
+        foreach ($commands as $command) {
+            if (!is_null($logger)) {
+                $logger->info("Executing " . $command);
+            }
+            $result = trim($connection->execute($command)->getOutput());
+
+            if (!is_null($logger)) {
+                $logger->info("Result: $result");
+            }
+        }
 
         $connection->disconnect();
         return $result;
@@ -25,21 +50,36 @@ trait SSHable
     /**
      * @throws \Throwable
      */
-    public function createSSHConnection(): Connection
+    public function createSSHConnection(QueueLogger $logger = null): Connection
     {
+
         throw_if(
             is_null($this->ip_addr) || is_null($this->password),
             new CannotConnectWithSSHException("Cannot create an SSH Connection. IP Address and Password information is missing.")
         );
 
+        if (is_null($this->password)) {
+            $this->password = "template1";
+        }
+        if (is_null($this->username)) {
+            $this->username = "root";
+        }
+
+
         $auth = new PasswordCredential($this->username, $this->password);
 
         $connection = new Connection(
             $this->ip_addr,
-            22, $auth);
+            22,
+            $auth
+        );
 
         if (!$connection->connect()) {
             throw new CannotConnectWithSSHException('Error connecting to the linux machine');
+        }
+
+        if (!is_null($logger)) {
+            $logger->info("Created an SSH Connection with " . $this->ip_addr);
         }
 
         if (!$connection->authenticate()) {
